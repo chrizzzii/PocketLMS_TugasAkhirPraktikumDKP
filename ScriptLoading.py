69386d3688b1ebd44e6e5f69b57ea49c5703d395
Agustinus Adven Christo <clvtito@gmail.com>
2022-05-28 11:43:31 +0700

    Versi satu

--- ScriptLoading.py
+++ ScriptLoading.py
@@ -1,25 +1,28 @@
-# Import Library & Class
+# Import Library
 from tkinter import *
 
- 
+# Set-up Library GUI 
 Loading = Tk()
 Loading.config(bg='#223441')
 Loading.overrideredirect(True)
 
+
+
+# Set-up Window Aplikasi Pocket LMS
 LebarJendela = 320
 TinggiJendela = 330
 
-# get the screen dimension
 LebarLayar = Loading.winfo_screenwidth()
 TinggiLayar = Loading.winfo_screenheight()
 
-# find the center point
-center_x = int(LebarLayar/2 - LebarJendela / 2)
-center_y = int(TinggiLayar/2 - TinggiJendela / 2)
+Tengah_x = int(LebarLayar/2 - LebarJendela / 2)
+Tengah_y = int(TinggiLayar/2 - TinggiJendela / 2)
 
-# set the position of the window to the center of the screen
-Loading.geometry(f'{LebarJendela}x{TinggiJendela}+{center_x}+{center_y}')
+Loading.geometry(f'{LebarJendela}x{TinggiJendela}+{Tengah_x}+{Tengah_y}')
 
+
+
+# Set-up UI
 Judul=Label(Loading, bg='#223441', fg='#F5F5F5', text="Pocket LMS", font='Arial 17 bold')
 Judul.place(relx=0.5, rely=0.5, anchor=CENTER)
 
